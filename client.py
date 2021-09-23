f2cb0244d6e63185885e6b4e088c951e97e3e567
Helio Potelicki <helio.p2000@hotmail.com>
2021-09-23 18:10:13 -0300

    update client

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ def startSendingTime(slave_client):
     while True:
         slave_client.send(str(datetime.datetime.now()).encode())
 
-        print("Recent time sent successfully", end="\n\n")
+        print("Hora enviada", end="\n\n")
         time.sleep(5)
 
 
@@ -19,9 +19,7 @@ def startReceivingTime(slave_client):
     while True:
         Synchronized_time = parser.parse(slave_client.recv(1024).decode())
 
-        print("Synchronized time at the client is: " + \
-                                    str(Synchronized_time),
-                                    end = "\n\n")
+        print(f"Hora sincronizada: {Synchronized_time}\n\n")
 
 
 def initiateSlaveClient(port=8080):
@@ -30,15 +28,14 @@ def initiateSlaveClient(port=8080):
 
     slave_client.connect(('127.0.0.1', port))
 
-    print("Starting to receive time from server\n")
+    print("Recebendo hora do servidor\n")
     send_time_thread = threading.Thread(target=startSendingTime,
                                         args=(slave_client, ))
     send_time_thread.start()
 
-    print("Starting to recieving " + \
-                         "synchronized time from server\n")
     receive_time_thread = threading.Thread(target=startReceivingTime,
                                            args=(slave_client, ))
+
     receive_time_thread.start()
 
 
